ae4cf5a2ff470ae1d27ef5c280ecfcfe47d69a9f
Mohammad Samian Yusuf <attackpiggy@users.noreply.github.com>
2019-03-29 11:39:26 -0400

    minor changes to file names

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='movies_index'),
-    path('<int:movie_id>', views.detail, name='movies_id')
+    path('<int:movie_id>', views.detail, name='movies_detail')
 ]
