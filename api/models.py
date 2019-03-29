56876e8b19665e152ce9de9a7f55268cf09b9094
Mohammad Samian Yusuf <attackpiggy@users.noreply.github.com>
2019-03-29 12:30:33 -0400

    fixed some small errors

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from tastypie.resources import ModelResource
 from movies.models import Movie
 
 
-class MovieResource():
+class MovieResource(ModelResource):
     class Meta:
         queryset = Movie.objects.all()
         resource_name = 'movies'
