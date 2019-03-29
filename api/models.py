61d6dd4bbaaa9c3750f68027763f7f52b7498b64
Mohammad Samian Yusuf <attackpiggy@users.noreply.github.com>
2019-03-29 12:20:07 -0400

    enabling conversation among main app and the api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from tastypie.resources import ModelResource
+from movies.models import Movie
 
-# Create your models here.
+
+class MovieResource():
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
